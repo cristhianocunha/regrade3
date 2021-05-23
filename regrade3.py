c33a4fc877c87947073a7c20eb897b91ade42d9b
CRISTHIANO RODRIGO CUNHA <cristhiano.cunha@gmail.com>
2021-05-22 22:25:31 -0300

    Add files via upload

---
+++ regrade3.py
@@ -0,0 +1,12 @@
+
+
+print("Regra de 3 simples")
+
+n1 = int(input ('Coloque primeiro Numero:  ')) 
+n2 = int(input ('Coloque segundo Numero:  ') )
+n3 = int (input ('Coloque terceiro Numero:  '))
+
+x1 = (n3 * n2)
+x  = x1/n1
+
+print (" resultado é: ", x )
